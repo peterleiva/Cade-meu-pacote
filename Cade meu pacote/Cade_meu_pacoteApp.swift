89cd08554c5119c4e86faf6119dae76aef4ddf49
Peter <contact@peter.dev.br>
2023-12-31 15:11:16 -0300

    Initial Commit

---
+++ Cade meu pacote/Cade_meu_pacoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cade_meu_pacoteApp.swift
+//  Cade meu pacote
+//
+//  Created by Peter on 31/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Cade_meu_pacoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
